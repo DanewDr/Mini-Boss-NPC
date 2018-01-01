4b6e67342ecd60c26a7607d6a4026011a5ef84a8
DanewDr <danen1701@gmail.com>
2018-01-01 12:14:14 +1100

    Add files via upload

--- MiniBossNPC.cs
+++ MiniBossNPC.cs
@@ -1,9 +1,18 @@
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace MiniBossNPC
 {
-	class MiniBossNPC : Mod
+	public class MiniBossNPC : Mod
 	{
+		public static Mod Instance;
+		public static Mod Tremor;
+		public static Mod Thorium;
+		public static Mod Spirit;
+		public static Mod calamityLoaded;
+
 		public MiniBossNPC()
 		{
 			Properties = new ModProperties()
@@ -13,5 +22,12 @@ namespace MiniBossNPC
 				AutoloadSounds = true
 			};
 		}
+		public override void PostSetupContent()
+		{
+			Tremor = ModLoader.GetMod("Tremor");
+			Thorium = ModLoader.GetMod("ThoriumMod");
+			Spirit = ModLoader.GetMod("SpiritMod");
+			calamityLoaded = ModLoader.GetMod("CalamityMod");
+		}		
 	}
 }
