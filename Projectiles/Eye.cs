08fcd12f360aa079a82e632adce2f1dcbdf2ec44
DanewDr <danen1701@gmail.com>
2018-09-30 22:00:08 +1000

    Add files via upload

---
+++ Projectiles/Eye.cs
@@ -0,0 +1,87 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+ 
+namespace MiniBossNPC.Projectiles
+{
+    public class Eye : ModProjectile
+    {
+		public override void SetStaticDefaults()
+			{
+				DisplayName.SetDefault("Eye");
+			}
+        public override void SetDefaults()
+        {
+            projectile.width = 30;
+            projectile.height = 20;
+            projectile.friendly = true;
+            projectile.aiStyle = 1;
+            projectile.penetrate = 1;      
+            projectile.extraUpdates = 1;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (projectile.velocity.X != oldVelocity.X)
+            {
+                projectile.velocity.X = -oldVelocity.X;
+            }
+            if (projectile.velocity.Y != oldVelocity.Y)
+            {
+                projectile.velocity.Y = -oldVelocity.Y;
+            }
+            return false;
+        }
+		public override void AI()
+		{
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 3.14f;
+			if (projectile.alpha > 70)
+			{
+				projectile.alpha -= 15;
+				if (projectile.alpha < 70)
+				{
+					projectile.alpha = 70;
+				}
+			}
+			if (projectile.localAI[0] == 0f)
+			{
+				AdjustMagnitude(ref projectile.velocity);
+				projectile.localAI[0] = 1f;
+			}
+			Vector2 move = Vector2.Zero;
+			float distance = 400f;
+			bool target = false;
+			for (int k = 0; k < 200; k++)
+			{
+				if (Main.npc[k].active && !Main.npc[k].dontTakeDamage && !Main.npc[k].friendly && Main.npc[k].lifeMax > 5)
+				{
+					Vector2 newMove = Main.npc[k].Center - projectile.Center;
+					float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+					if (distanceTo < distance)
+					{
+						move = newMove;
+						distance = distanceTo;
+						target = true;
+					}
+				}
+			}
+			if (target)
+			{
+				AdjustMagnitude(ref move);
+				projectile.velocity = (10 * projectile.velocity + move) / 11f;
+				AdjustMagnitude(ref projectile.velocity);
+			}
+		}
+
+		private void AdjustMagnitude(ref Vector2 vector)
+		{
+			float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+			if (magnitude > 6f)
+			{
+				vector *= 6f / magnitude;
+			}
+		}	
+    }
+}
